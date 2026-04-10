e2d124ae2ed46d96cbd7a714781d52d168add564
PAVANI533 <sreep675@gmail.com>
2026-04-10 10:19:59 +0530

    finall changes

--- placement_system/settings.py
+++ placement_system/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2zibx1qdtlb&(%@*pc669jl8y^(z38j35o&tdaxve(unu@pwsm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
