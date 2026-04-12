99ce6edd0248561789b7c1b47ad606fe2de923dc
PAVANI533 <sreep675@gmail.com>
2026-04-12 15:10:30 +0530

    fixed database config

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn placement_system.wsgi
+web: gunicorn placement_system.wsgi --bind 0.0.0.0:$PORT
--- placement_system/settings.py
+++ placement_system/settings.py
@@ -74,14 +74,20 @@ WSGI_APPLICATION = 'placement_system.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-import dj_database_url
 import os
+import dj_database_url
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL')
-    )
-}
+if os.environ.get('DATABASE_URL'):
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
