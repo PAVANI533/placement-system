c9a8c9c87b11fe9fa7bcdf2138206bf9c7e51f80
PAVANI533 <sreep675@gmail.com>
2026-04-13 09:52:06 +0530

    latest

--- placement_system/settings.py
+++ placement_system/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 '#django-insecure-2zibx1qdtlb&(%@*pc669jl8y^(z38j35o&tdaxve(unu@pwsm'
 SECRET_KEY = os.environ.get("SECRET_KEY", 'fallback-secret')
-DEBUG = False
+DEBUG = True
 # SECURITY WARNING: don't run with debug turned on in production!
 
 
